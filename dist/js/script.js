9aa197df6fd9eb19dd30ff2bbbc14102a75b878e
tsutrof <jaysonfortus0815@gmail.com>
2023-06-06 10:52:19 +0800

    penge hehe

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,19 +5,18 @@ const circle = document.querySelector(".circle-toggle__menu");
 const circleNav = document.querySelector(".mobile-contact");
 const header = document.querySelector(".header");
 toggleMenu.addEventListener("click", () => {
-    toggleMenu.classList.toggle("open");
+	toggleMenu.classList.toggle("open");
 	mobileNav.classList.toggle("open");
-    header.classList.toggle("open");
+	header.classList.toggle("open");
 
 	circle.classList.remove("open");
-    circleNav.classList.remove("open");
+	circleNav.classList.remove("open");
 });
-
 
 /* CIRCLE TOGGLE */
 circle.addEventListener("click", () => {
-    circle.classList.toggle("open");
-    circleNav.classList.toggle("open");
+	circle.classList.toggle("open");
+	circleNav.classList.toggle("open");
 
 	toggleMenu.classList.remove("open");
 	mobileNav.classList.remove("open");
@@ -31,7 +30,6 @@ links.forEach((baba) => {
 		baba.querySelector("i").classList.toggle("open");
 	});
 });
-
 
 /* HORIZON TAB */
 const horizonNav = document.querySelectorAll(".horizon-tab__link");
@@ -57,4 +55,26 @@ function removeActiveContent() {
 	horizonContent.forEach((horizon) => {
 		horizon.classList.remove("active");
 	});
+}
+
+// Get the button
+let mybutton = document.getElementById("roll-up");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 600) {
+		mybutton.style.display = "block";
+	} else {
+		mybutton.style.display = "none";
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
 }
