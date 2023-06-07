7ac42f2eb9267c2cb017a467de9bd4486d036fd1
tsutrof <jaysonfortus0815@gmail.com>
2023-06-07 08:49:35 +0800

    complete

--- dist/js/script.js
+++ dist/js/script.js
@@ -57,10 +57,9 @@ function removeActiveContent() {
 	});
 }
 
-// Get the button
+// SCROLL TO TOP
 let mybutton = document.getElementById("roll-up");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
 	scrollFunction();
 };
@@ -73,7 +72,6 @@ function scrollFunction() {
 	}
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
